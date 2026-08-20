72e25526418934d343a8d90e5a64fdbe6e29ea48
Yogesh ginti <163818028+Yogesh-Ginti@users.noreply.github.com>
2026-08-20 18:49:45 +0530

    Initialize VAST server with Express and routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const vastFile = path.join(__dirname, "vast.xml");
+
+
+app.get("/", (req, res) => {
+  res.send("VAST server is running!");
+});
+
+app.get("/vast", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://imasdk.googleapis.com");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendFile(vastFile);
+});
+
+app.listen(PORT, () => {
+  console.log(`VAST server running at http://localhost:${PORT}/vast`);
+});
